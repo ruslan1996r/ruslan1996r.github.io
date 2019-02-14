0258d9f04bcd88046533ffd2205b9521f4aef6f4
Ruslan Yusufov <ruslan96218@gmail.com>
2019-02-14 15:36:15 +0200

    first commit

---
+++ precache-manifest.27c813f70b54fcaa66da50845ea50b22.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "786bb12823bd48b7befef8ba1cf75584",
+    "url": "/static/media/footer-instagram.786bb128.svg"
+  },
+  {
+    "revision": "9c32cfe23d96d4707fed",
+    "url": "/static/css/main.267be703.chunk.css"
+  },
+  {
+    "revision": "64509f9465251a63f850",
+    "url": "/static/js/1.64509f94.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fa3e2a36a7813239a0877d37891d7362",
+    "url": "/static/media/header-facebook.fa3e2a36.svg"
+  },
+  {
+    "revision": "5b5e2bd8881285dacb9f39ec6987bf76",
+    "url": "/static/media/header-instagram.5b5e2bd8.svg"
+  },
+  {
+    "revision": "182cafa1b330704dc3d4a0d976dafd8f",
+    "url": "/static/media/header-telegram.182cafa1.svg"
+  },
+  {
+    "revision": "ba34ce4314073aeeb891436344d98b5c",
+    "url": "/static/media/footer-facebook.ba34ce43.svg"
+  },
+  {
+    "revision": "9c32cfe23d96d4707fed",
+    "url": "/static/js/main.9c32cfe2.chunk.js"
+  },
+  {
+    "revision": "5132fda8319b2635396d35f2624ca57b",
+    "url": "/static/media/footer-telegram.5132fda8.svg"
+  },
+  {
+    "revision": "3241884f076ff28ee351522d0aeb6a20",
+    "url": "/static/media/clip.3241884f.svg"
+  },
+  {
+    "revision": "55d06a4ebb8737f1b158ac21beaa5660",
+    "url": "/static/media/course-artboard.55d06a4e.svg"
+  },
+  {
+    "revision": "d29f1779664178a55ed87236fd97c302",
+    "url": "/static/media/success.d29f1779.svg"
+  },
+  {
+    "revision": "d22d46f30591a840d2c4f215191de8f1",
+    "url": "/static/media/gallery-bg.d22d46f3.svg"
+  },
+  {
+    "revision": "1a0574158921318262be982e4e179992",
+    "url": "/static/media/left-arrow.1a057415.svg"
+  },
+  {
+    "revision": "ed9bad821d5aaeb314b7afab1555c359",
+    "url": "/index.html"
+  }
+];
